a22de93d222980aeac583ff0c0238e8e22a69997
Meda840 <91147472+Meda840@users.noreply.github.com>
2024-06-12 00:40:25 +0200

    init

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+const config = {
+  api_base_url: "https://nadeats.fr",
+  api_token:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJuYWRlYXRzLmZyIiwic3ViIjoiYzkyZTA4MjEtMGM3Ny0xMWVmLWE5Y2EtMjU3M2QzY2E3OGZjIiwiaWF0IjoxNzE1MDg5NDA4fQ.u2JvCSrvETmI26uaM05N02WYJEq0UYUUKIGvgyQjCAg",
+  google_client_id:
+    "46158489047-vptkm75h1okppmo5k0btqor6n8hki0dp.apps.googleusercontent.com",
+  facebook_app_id: "361247692401530",
+  topic: "customer",
+  channel: "krms-channel",
+  sound: "notify.mp3",
+};
+export default config;
